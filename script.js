7c56df51a406090719fd280010ad368d40159f20
aqifsariman <aqifsariman@gmail.com>
2021-12-06 20:16:55 +0800

    Added game logic

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-// keep data about the game in a 2-D array
 const board = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
-];
-
-// the element that contains the rows and squares
-let boardElement;
-
-// the element that contains the entire board
-// we can empty it out for convenience
-let boardContainer;
-
-// current player global starts at X
-let currentPlayer = 'X';
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]; // keep data about the game in a 2-D array
+let boardElement; // the element that contains the rows and squares
+let boardContainer; // the element that contains the entire board // we can empty it out for convenience
+let currentPlayer = "X"; // current player global starts at X
+const gameMessage = document.querySelector(".game-message");
 
 // completely rebuilds the entire board every time there's a click
 const buildBoard = (board) => {
   // start with an empty container
-  boardContainer.innerHTML = '';
-  boardElement = document.createElement('div');
-  boardElement.classList.add('board');
+  boardContainer.innerHTML = "";
+  boardElement = document.createElement("div");
+  boardElement.classList.add("board");
 
-  // move through the board data array and create the
-  // current state of the board
   for (let i = 0; i < board.length; i += 1) {
-    // separate var for one row / row element
-    const row = board[i];
-    const rowElement = document.createElement('div');
-    rowElement.classList.add('row');
+    // move through the board data array and create the current state of the board
+    const row = board[i]; // separate var for one row / row element
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("row");
 
     // set each square
     // j is the column number
     for (let j = 0; j < row.length; j += 1) {
       // one square element
-      const square = document.createElement('div');
-      square.classList.add('square');
+      const square = document.createElement("div");
+      square.classList.add("square");
 
       // set the text of the square according to the array
       square.innerText = board[i][j];
@@ -44,7 +35,7 @@ const buildBoard = (board) => {
 
       // set the click all over again
       // eslint-disable-next-line
-      square.addEventListener('click', () => {
+      square.addEventListener("click", () => {
         squareClick(i, j);
       });
     }
@@ -52,11 +43,12 @@ const buildBoard = (board) => {
     // add a single row to the board
     boardContainer.appendChild(rowElement);
   }
+  return boardContainer;
 };
 
 // create the board container element and put it on the screen
 const initGame = () => {
-  boardContainer = document.createElement('div');
+  boardContainer = document.createElement("div");
   document.body.appendChild(boardContainer);
 
   // build the board - right now it's empty
@@ -65,28 +57,110 @@ const initGame = () => {
 
 // switch the global values from one player to the next
 const togglePlayer = () => {
-  if (currentPlayer === 'X') {
-    currentPlayer = 'O';
-  } else {
-    currentPlayer = 'X';
+  if (currentPlayer === "X") {
+    gameMessage.innerText = "Player 2's turn";
+    currentPlayer = "O";
+  } else if (currentPlayer === "O") {
+    gameMessage.innerText = "Player 1's turn";
+    currentPlayer = "X";
   }
 };
-
 const squareClick = (column, row) => {
-  console.log('coordinates', column, row);
+  console.log("coordinates", column, row);
 
   // see if the clicked square has been clicked on before
-  if (board[column][row] === '') {
+  if (board[column][row] === "") {
     // alter the data array, set it to the current player
     board[column][row] = currentPlayer;
+    console.log(board);
 
+    if (checkWin(board) === true) {
+      gameMessage.innerHTML = `${currentPlayer} player wins!`;
+    } else {
+      // change the player
+      togglePlayer();
+    }
+    // if checkwin false and all squares filled up, its a draw
+    if (
+      checkWin(board) === false &&
+      board[0][0] !== "" &&
+      board[1][0] !== "" &&
+      board[2][0] !== "" &&
+      board[0][1] !== "" &&
+      board[0][2] !== "" &&
+      board[1][1] !== "" &&
+      board[1][2] !== "" &&
+      board[2][1] !== "" &&
+      board[2][2] !== ""
+    ) {
+      gameMessage.innerHTML = "It's a draw!";
+    }
     // refresh the screen with a new board
     // according to the array that was just changed
     buildBoard(board);
-
-    // change the player
-    togglePlayer();
   }
 };
 
-initGame()
+const checkWin = (board) => {
+  // check every position
+  // there is a conditional for all 15 win conditions
+  if (
+    board[0][0] !== "" &&
+    board[0][0] === board[0][1] &&
+    board[0][1] === board[0][2]
+  ) {
+    return true;
+  }
+  if (
+    board[1][0] !== "" &&
+    board[1][0] === board[1][1] &&
+    board[1][1] === board[1][2]
+  ) {
+    return true;
+  }
+  if (
+    board[2][0] !== "" &&
+    board[2][0] === board[2][1] &&
+    board[2][1] === board[2][2]
+  ) {
+    return true;
+  }
+  if (
+    board[0][0] !== "" &&
+    board[0][0] === board[1][0] &&
+    board[1][0] === board[2][0]
+  ) {
+    return true;
+  }
+  if (
+    board[0][1] !== "" &&
+    board[0][1] === board[1][1] &&
+    board[1][1] === board[2][1]
+  ) {
+    return true;
+  }
+  if (
+    board[0][2] !== "" &&
+    board[0][2] === board[1][2] &&
+    board[1][2] === board[2][2]
+  ) {
+    return true;
+  }
+  if (
+    board[0][0] !== "" &&
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2]
+  ) {
+    return true;
+  }
+  if (
+    board[2][0] !== "" &&
+    board[2][0] === board[1][1] &&
+    board[1][1] === board[0][2]
+  ) {
+    return true;
+  }
+  return false;
+};
+
+initGame();
